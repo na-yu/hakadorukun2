8eeddfa4faaf60b4c58c9c52fccfb03a33692e7a
na-yu <yukinakai1027@gmail.com>
2021-09-10 15:41:45 +0900

    test

--- pscweb2/settings.py
+++ pscweb2/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_URL = 'user_app:login'
