d1813efa755a3efc98b7138069e60671bc9e0647
na-yu <yukinakai1027@gmail.com>
2021-09-10 15:16:25 +0900

    test

--- pscweb2/settings.py
+++ pscweb2/settings.py
@@ -96,8 +96,7 @@ WSGI_APPLICATION = 'pscweb2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 
